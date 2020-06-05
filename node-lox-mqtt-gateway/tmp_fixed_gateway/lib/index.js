af4f32b327bb1be706a0039a19c022f492563582
Your Name <you@example.com>
2020-06-05 11:52:28 +0200

    using own fork of nodeloxmqttgateway, updated quickstart, ...

--- node-lox-mqtt-gateway/tmp_fixed_gateway/lib/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "Adaptor": require("./Adaptor.js"),
-    "Logger": require("./Logger.js"),
-    "mqtt_builder": require("./mqtt_builder.js"),
-    "App": require("./App.js"),
-    "WebSocketAPI": require("./WebSocketAPI.js"),
-    "Structure": require("node-lox-structure-file"),
-};
