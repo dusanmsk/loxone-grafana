f3a1497e08aaeb126ad1c7fac28cd64ac463b98e
Dusan Zatkovsky <dusanmsk@gmail.com>
2020-06-12 12:36:35 +0200

    misc updates
    - autoreconnect
    - more properties than hardcode

--- loxone2influx/loxone2influx.groovy
+++ loxone2influx/loxone2influx.groovy
@@ -1,11 +1,10 @@
-@Grab(group = 'org.eclipse.paho', module = 'mqtt-client', version = '0.4.0')
+@Grab("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.4")
 @Grab("org.influxdb:influxdb-java:2.7")
 @Grab(group = 'org.slf4j', module = 'slf4j-api', version = '1.6.1')
 @Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '0.9.28')
 
 import groovy.json.JsonSlurper
 import groovy.util.logging.Slf4j
-
 import org.eclipse.paho.client.mqttv3.*
 import org.influxdb.InfluxDB
 import org.influxdb.InfluxDBFactory
@@ -16,11 +15,9 @@ import java.util.concurrent.TimeUnit
 @Slf4j
 class Main {
 
-    // TODO config file
-    def FIRE_EVEN_NOT_CHANGED_SEC = 1800        // each 30 min refire stalled values
-    def RECONNECT_TIME_SEC = 30
-
-    def mqttUrl = "tcp://mosquitto:1883"
+    def FIRE_EVEN_NOT_CHANGED_SEC = System.getenv("FIRE_EVEN_NOT_CHANGED_SEC").toInteger()
+    def MQTT_ADDRESS = System.getenv("MQTT_ADDRESS")
+    def LOXONE_MQTT_TOPIC_NAME = System.getenv("LOXONE_MQTT_TOPIC_NAME")
 
     def jsonSlurper = new JsonSlurper()
     InfluxDB influxDB
@@ -33,60 +30,44 @@ class Main {
 
     def start() throws Exception {
         refireThread.start()
-        def connected = false
-        while (true) {
-            try {
-                if (!connected) {
-                    log.info("Connecting ...")
+        log.info("Connecting to ${MQTT_ADDRESS}")
+        log.info("FIRE_EVEN_NOT_CHANGED_SEC=${FIRE_EVEN_NOT_CHANGED_SEC}")
 
-                    influxDB = InfluxDBFactory.connect("http://influxdb:8086", "grafana", "grafana")
-                    influxDB.createDatabase(dbName)
-                    influxDB.setDatabase(dbName)
-                    log.info("Connected to influx")
+        influxDB = InfluxDBFactory.connect("http://influxdb:8086", "grafana", "grafana")
+        influxDB.createDatabase(dbName)
+        influxDB.setDatabase(dbName)
+        log.info("Connected to influx")
 
-                    client = new MqttClient(mqttUrl, "mqtt2influx", null)
-                    client.connect()
+        client = new MqttClient(MQTT_ADDRESS, "mqtt2influx", null)
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions()
+        mqttConnectOptions.setAutomaticReconnect(true)
+        mqttConnectOptions.setMaxReconnectDelay(60)
+        client.connect(mqttConnectOptions)
 
-                    client.setCallback(new MqttCallback() {
-                        @Override
-                        void connectionLost(Throwable throwable) {
-                            log.warn "MQTT connection lost. Will reconnect in ${RECONNECT_TIME_SEC} seconds."
-                            connected = false
-                        }
-
-                        @Override
-                        void messageArrived(String topic, MqttMessage mqttMessage) {
-                            try {
-                                processMessage(topic, new String(mqttMessage.payload))
-                            } catch (Exception e) {
-                                e.printStackTrace()
-                            }
-                        }
-
-                        @Override
-                        void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {}
-                    })
-
-                    client.subscribe("lox/#")
-                    log.info("Connected to mqtt")
-                    log.info("Ready")
-                    connected = true
-                }
-            } catch (Exception e) {
-                log.error("Exception, disconnecting. Will reconnect in ${RECONNECT_TIME_SEC} seconds", e)
-                if (influxDB) {
-                    influxDB.close()
-                }
-                if (client) {
-                    client.close()
-                }
-                connected = false
+        client.setCallback(new MqttCallback() {
+            @Override
+            void connectionLost(Throwable throwable) {
+                log.warn "MQTT connection lost. Will try to reconnect automatically."
             }
 
-            Thread.sleep(RECONNECT_TIME_SEC * 1000);
-        }
+            @Override
+            void messageArrived(String topic, MqttMessage mqttMessage) {
+                try {
+                    processMessage(topic, new String(mqttMessage.payload))
+                } catch (Exception e) {
+                    e.printStackTrace()
+                }
+            }
 
+            @Override
+            void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {}
+        })
+
+        client.subscribe("${LOXONE_MQTT_TOPIC_NAME}/#")
+        log.info("Connected to mqtt")
+        log.info("Ready")
     }
+
 
     def getStatName(topic) {
         def s = topic.toString().replace("lox/", "").replace("/state", "").replaceAll("[^A-Za-z0-9_]", "_");
