870c4913ffca736bd0f51516da54f316a5f380ca
Your Name <you@example.com>
2020-10-26 15:13:59 +0100

    better migration procedure with progress

--- util/string2float_fix/convert.groovy
+++ util/string2float_fix/convert.groovy
@@ -1,23 +1,29 @@
+import com.opencsv.CSVIterator
+import com.opencsv.CSVReader
 @Grab("org.influxdb:influxdb-java:2.20")
 @Grab(group = 'org.slf4j', module = 'slf4j-api', version = '1.6.1')
 @Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '0.9.28')
 @Grab('com.opencsv:opencsv:4.0')
 import groovy.util.logging.Slf4j
-import org.eclipse.paho.client.mqttv3.*
 import org.influxdb.InfluxDB
 import org.influxdb.InfluxDBFactory
 import org.influxdb.dto.Point
 import org.influxdb.dto.Query
-import org.influxdb.dto.QueryResult
-import org.influxdb.impl.TimeUtil
 
 import java.util.concurrent.TimeUnit
-import java.util.function.Consumer
 
 @Slf4j
 class Main {
 
+    final SRC_INFLUX_ADDRESS = "srcinfluxdb"
     final SRC_INFLUX_PORT = 8086
+
+    //final SRC_INFLUX_ADDRESS = "localhost"
+    //final SRC_INFLUX_PORT = 8087
+
+
+    final DST_INFLUX_ADDRESS = "influxdb"
+    //final DST_INFLUX_ADDRESS = "localhost"
     final DST_INFLUX_PORT = 8086
 
     def IGNORED_COLUMN_NAMES = ['name', 'time']
@@ -27,11 +33,11 @@ class Main {
     def start() {
 
         log.info("Convertor started")
-        srcInfluxDB = InfluxDBFactory.connect("http://srcinfluxdb:${SRC_INFLUX_PORT}", "grafana", "grafana")
+        srcInfluxDB = InfluxDBFactory.connect("http://${SRC_INFLUX_ADDRESS}:${SRC_INFLUX_PORT}", "grafana", "grafana")
         srcInfluxDB.setDatabase("loxone")
         log.info("Connected to source influx")
 
-        dstInfluxDB = InfluxDBFactory.connect("http://influxdb:${DST_INFLUX_PORT}", "grafana", "grafana")
+        dstInfluxDB = InfluxDBFactory.connect("http://${DST_INFLUX_ADDRESS}:${DST_INFLUX_PORT}", "grafana", "grafana")
         dstInfluxDB.createDatabase("loxone")
         dstInfluxDB.setDatabase("loxone")
         dstInfluxDB.enableBatch(2000, 1000, TimeUnit.MILLISECONDS)
@@ -40,8 +46,11 @@ class Main {
         def measurements = getMeasurementNames(srcInfluxDB)
         
         log.info("Migrating data")
+        def remainingMeasurements = measurements.size()
         measurements.each { measurement ->
+            log.info("Processing measurement ${measurement}, remaining ${remainingMeasurements}")
             processMeasurement(measurement)
+            remainingMeasurements--
         }
 
     }
@@ -61,50 +70,20 @@ class Main {
     }
 
     def processMeasurement(measurementName) {
-        log.info("Processing measurement: $measurementName")
-        srcInfluxDB.query(new Query("select * from ${measurementName}", "loxone"), 1000, new Consumer<QueryResult>() {
-            @Override
-            void accept(QueryResult queryResult) {
-                queryResult.results.each { result ->
-                    result.series.each { serie ->
-                        serie.values.each { valueList ->
-                            def valueMap = convertValuesToMap(serie.columns, valueList)
-                            def time = TimeUtil.fromInfluxDBTimeFormat(valueMap.remove("time"))
-                            def pointBuilder = Point.measurement(measurementName).time(time, TimeUnit.MILLISECONDS)
-                            def fieldCount = 0
-                            valueMap.keySet().each { key ->
-                                def value = (key in IGNORED_COLUMN_NAMES) ? value : fixupValue(valueMap[key])
-                                if (value != null) {
-                                    if(value instanceof String) { log.warn("${measurementName}.${key} is string (${value}), original value is (${valueMap[key]})"); }
-                                    pointBuilder = pointBuilder.addField(key, value)
-                                    fieldCount++
-                                }
-                            }
-                            pointProcessed()
-                            if (fieldCount > 0) {
-                                Point point = pointBuilder.build();
-                                dstInfluxDB.write(point)
-                            } else {
-                                log.warn("Zero fields for $valueList for $measurementName")
-                            }
-                        }
-                    }
-                }
+        exec("""echo "select * from ${measurementName}" | influx -host ${SRC_INFLUX_ADDRESS} -port ${SRC_INFLUX_PORT} -database loxone -format csv >/tmp/dump.csv""").text
+
+        log.info("Writing fixed data to influx")
+        CSVIterator iterator = new CSVIterator(new CSVReader(new FileReader("/tmp/dump.csv")));
+        def header = null
+        for(String[] nextLine : iterator) {
+            if(header == null) {
+                header = nextLine
+            } else {
+                def fieldMap = extractCsvLine(header, nextLine)
+                writeValues(fieldMap)
             }
-        })
-    }
-
-    def processedPoints = 0
-    def lastPrintTimestamp = System.currentTimeMillis()
-
-    def pointProcessed() {
-        processedPoints++
-        if (System.currentTimeMillis() - lastPrintTimestamp > 10000) {
-            log.info "Processed $processedPoints points"
-            lastPrintTimestamp = System.currentTimeMillis()
         }
     }
-
     def convertValuesToMap(List fieldNames, List values) {
         def aMap = [:]
         for (int i = 0; i < fieldNames.size(); i++) {
@@ -149,9 +128,46 @@ class Main {
         return null
     }
 
+    def exec(cmd) {
+        def command=["/bin/bash", "-c", cmd]
+        log.debug("Executing ${command}")
+        return command.execute()
+    }
+
+    def extractCsvLine(String[] header, String[] fields) {
+        def ret = [:]
+        for (i in 0..<header.size()) {
+            ret[header[i]] = fields[i]
+        }
+        return ret
+    }
+
+    def writeValues(valueMap) {
+        def measurementName = valueMap.remove("name")
+        def time = valueMap.remove("time") as Long
+        def pointBuilder = Point.measurement(measurementName).time(time, TimeUnit.NANOSECONDS)
+        def fieldCount = 0
+        valueMap.keySet().each { key ->
+            def originalValue = valueMap[key]
+            def fixedValue = (key in IGNORED_COLUMN_NAMES) ? originalValue : fixupValue(originalValue)
+            if (fixedValue != null) {
+                if(fixedValue instanceof String) { log.warn("${measurementName}.${key} is string (${fixedValue}), original value is (${originalValue})"); }
+                pointBuilder = pointBuilder.addField(key, fixedValue)
+                fieldCount++
+            }
+        }
+        if (fieldCount > 0) {
+            Point point = pointBuilder.build();
+            dstInfluxDB.write(point)
+        } else {
+            log.warn("Zero fields for $valueMap for $measurementName")
+        }
+    }
+
 }
 
 
 def m = new Main()
 m.start()
+
 
