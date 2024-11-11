d07ded2eb461a35d96411d8e7c3d5568f8d74287
Dušan Zatkovský <dusan.zatkovsky@tipsport.cz>
2024-11-11 11:18:07 +0100

    configuration changes, more configuration properties and config value consistency

--- run.sh
+++ run.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+
+# configuration variants:
+#
+# 1. run only loxone2mqtt
+#   docker-compose up [-d] [--build] node-lox-mqtt-gw mosquitto
+#
+# 2. run loxone2mqtt with influxdb and grafana
+#   docker-compose up [-d] [--build] node-lox-mqtt-gw mosquitto grafana influxdb loxone2influx
+#
+# 3. run loxone2mqtt with timescaledb and grafana
+#       
+
 docker-compose up $@
