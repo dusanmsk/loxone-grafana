950a106c1dcf1ee9c8e70eb062f55698006038c3
Dusan Zatkovsky <dusanmsk@gmail.com>
2024-11-22 21:48:40 +0100

    ...

--- loxone2questdb/migrate_influxdb2questdb2.py
+++ loxone2questdb/migrate_influxdb2questdb2.py
@@ -116,17 +116,17 @@ def convertField(key, value, influx_type, questdb_type):
         except ValueError:
             return f"{key}_str", str(value)
 
-def convertTypes(fields, influx_field_types, questdb_field_types):
-    columns = {}
-    for key, value in fields.items():
-        influx_type = influx_field_types.get(key)
-        questdb_type = questdb_field_types.get(key)
-        if(type_match(influx_type, questdb_type)):
-            columns[key] = value
-        else:
-            key, value = convertField(key, value, influx_type, questdb_type)
-            columns[key] = value
-    return columns
+# def convertTypes(fields, influx_field_types, questdb_field_types):
+#     columns = {}
+#     for key, value in fields.items():
+#         influx_type = influx_field_types.get(key)
+#         questdb_type = questdb_field_types.get(key)
+#         if(type_match(influx_type, questdb_type)):
+#             columns[key] = value
+#         else:
+#             key, value = convertField(key, value, influx_type, questdb_type)
+#             columns[key] = value
+#     return columns
 
 
 def insert_chunk_into_questdb(measurement_name, chunk, influx_field_types, questdb_field_types):
